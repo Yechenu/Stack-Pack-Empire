c926bf127a64b5268cfbceb8006a5a1d02e5b804
Gem Code <diamondbrown79@gmail.com>
2023-01-13 14:16:41 -0500

    Uploading Maven project folder into  respository

---
+++ ADP_Capstone/src/main/java/com/okta/developer/ADP_Capstone/AdpCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.okta.developer.ADP_Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpCapstoneApplication.class, args);
+	}
+
+}
