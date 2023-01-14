fe6691fba4197e4771a16cdcf954362ed15065fe
Gem Code <diamondbrown79@gmail.com>
2023-01-13 22:39:24 -0500

    Revised DocumentDAO.java:

--- ADP_Capstone/src/main/java/com/dao/DocumentDAO.java
+++ ADP_Capstone/src/main/java/com/dao/DocumentDAO.java
@@ -1,7 +1,5 @@
 package com.dao;
 
-
-
 //import model and conn
 import com.model.Document;
 
@@ -17,12 +15,23 @@ import javax.servlet.http.HttpServlet;
 
 import java.io.FileNotFoundException;
 
-
+/* DocuementDAO.java 
+ * This DAO class provides CRUD database operations for 
+ * the table document in the database.
+ * 
+ *  Author: @Diamond Brown
+ */
 //insert document info into Document table
 public class DocumentDAO extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-		//INPUT DOCUMENT INFO: write method that returns a string. This method will pass the Document object)
+	private static  final String INSERT_DOCUMENT_SQL = "INSERT INTO document (ListType, "
+								+ "DocumentType, IssuingAuthority, ExpirationDate"
+								+ "DocumentPdf) values (?,?,?,?,?)";
+
+		/*INPUT DOCUMENT INFO: write method that returns a string. This method will
+		 * pass the Document object)
+		 */
 		public int InputFile (Document model)
 				throws ClassNotFoundException, FileNotFoundException{
 			int rowCount = 0;
@@ -33,9 +42,7 @@ public class DocumentDAO extends HttpServlet {
 			try {
 				//Instantiate PreparedStatement object. Allows you to send statements to MySql
 				PreparedStatement pst= 
-						conn.prepareStatement("INSERT INTO documents (ListType, "
-								+ "DocumentType, IssuingAuthority, ExpirationDate"
-								+ "DocumentPdf) values (?,?,?,?,?)");  //<-- the ???? are placeholders
+						conn.prepareStatement(INSERT_DOCUMENT_SQL);  //<-- the ???? are placeholders
 				
 				// the get methods replace the space holder fields ^^^
 				pst.setString(1, model.getListType());    
@@ -43,7 +50,7 @@ public class DocumentDAO extends HttpServlet {
 				pst.setString(3, model.getIssuingAuthority());
 				pst.setString(4, model.getExpirationDate());
 				
-				//if there is a file inputed...
+				//if there IS a  file ...
 				if (model.getDocumentPdf() != null) { // note: add a delete file feature for the document upload.
 					//fetch input stream of the uploaded file and set as a blob data type
 					pst.setBlob(5, model.getDocumentPdf());
